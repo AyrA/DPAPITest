bfe5ed565ce3d56aed2453b9d430a350fab7af31
AyrA <admin@ayra.ch>
2020-10-17 17:16:50 +0200

    Adding icon

--- DPAPITest/Properties/AssemblyInfo.cs
+++ DPAPITest/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DPAPITest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("DP API Tester")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DPAPITest")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © Kevin Gut 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- DPAPITest/frmMain.Designer.cs
+++ DPAPITest/frmMain.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.lblIntro = new System.Windows.Forms.Label();
             this.gbStep1 = new System.Windows.Forms.GroupBox();
             this.btnStep1 = new System.Windows.Forms.Button();
@@ -293,6 +294,7 @@
             this.Controls.Add(this.gbStep1);
             this.Controls.Add(this.lblIntro);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "frmMain";
             this.Text = "DP API Test";
